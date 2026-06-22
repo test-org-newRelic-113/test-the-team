f5a8772979715cecbe535159d4720f5742db2f2a
Chandradeep <csuvarnakanti@newrelic.com>
2026-06-22 14:08:48 +0530

    Add 5 more vulnerable dependencies (Velocity, Fastjson, BeanUtils, dom4j, Spring4Shell)

--- src/main/java/com/example/vulnerable/VulnerableApp.java
+++ src/main/java/com/example/vulnerable/VulnerableApp.java
@@ -11,6 +11,11 @@ import com.thoughtworks.xstream.XStream;
 import org.yaml.snakeyaml.Yaml;
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.apache.velocity.app.VelocityEngine;
+import com.alibaba.fastjson.JSON;
+import org.apache.commons.beanutils.BeanUtils;
+import org.dom4j.DocumentHelper;
+import org.springframework.web.servlet.DispatcherServlet;
 import java.security.Security;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -85,8 +90,24 @@ public class VulnerableApp {
         Security.addProvider(new BouncyCastleProvider());
         logger.info("Bouncy Castle provider registered (vulnerable version 1.65)");
 
+        // Using Apache Velocity (SSTI/RCE - CVE-2020-13936)
+        VelocityEngine velocityEngine = new VelocityEngine();
+        logger.info("Apache Velocity engine initialized (SSTI vulnerable): {}", velocityEngine.getClass().getName());
+
+        // Using Fastjson (RCE via deserialization - CVE-2019-14540)
+        logger.info("Fastjson version loaded: {}", JSON.VERSION);
+
+        // Using Commons BeanUtils (class traversal - CVE-2019-10086)
+        logger.info("Commons BeanUtils loaded: {}", BeanUtils.class.getName());
+
+        // Using dom4j (XXE injection - CVE-2018-1000632)
+        logger.info("dom4j DocumentHelper loaded: {}", DocumentHelper.class.getName());
+
+        // Using Spring WebMVC (Spring4Shell RCE - CVE-2022-22965)
+        logger.info("Spring WebMVC DispatcherServlet loaded: {}", DispatcherServlet.class.getName());
+
         logger.info("Application running with multiple vulnerable dependencies!");
-        logger.info("Total vulnerable dependencies: 16");
+        logger.info("Total vulnerable dependencies: 21");
     }
 
     /**
